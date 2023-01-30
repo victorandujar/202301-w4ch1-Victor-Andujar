57e304a5326e888af8b18537cf57846b97ae5962
Víctor Andújar <victorandujarmurcia@gmail.com>
2023-01-30 20:29:45 +0100

    Create button component

---
+++ components.md
@@ -0,0 +1,17 @@
+# Data
+
+## Data layer
+
+## Data modifications
+
+# Components
+
+## App
+
+- Header component
+
+## Card
+
+## Info
+
+## Button
---
+++ src/components/ButtonComponent/ButtonComponent.tsx
@@ -0,0 +1,5 @@
+const ButtonComponent = (): JSX.Element => {
+  return <button className="button button--select">Select all</button>;
+};
+
+export default ButtonComponent;
